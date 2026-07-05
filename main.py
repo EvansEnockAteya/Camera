dae068a44c7f0e5b79164d4e5225e78891aaea1a
EvansEnockAteya <evans_enock@outlook.com>
2026-07-05 11:13:45 +0300

    Added a navigation pannel. Though it has no functionaliti at the moment

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+
 from kivymd.app import MDApp
 from kivy.lang import Builder
 
+from kivy.clock import Clock
 
 class MyApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.primary_palette = "Green"
         self.theme_cls.theme_style = "Light"
-        return Builder.load_file("screens/main.kv")
+        
+        root = Builder.load_file("screens/main.kv")
+        Clock.schedule_once(lambda dt: root.ids.nav_drawer.set_state("closed"), 0.5)
+        
+        return root
 
     def say_hello(self):
-        self.root.ids.greeting.text = "Hi there! Welcome to KivyMD!"
+        output = "Hi there! Welcome to KivyMD!"
+        self.root.ids.greeting.text = output
 
 
 MyApp().run()
